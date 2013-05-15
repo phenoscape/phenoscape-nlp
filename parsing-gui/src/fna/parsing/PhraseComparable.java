9f9af73ccd0227645e6aabb61864c61bbcbf85de
hongcui <hong1.cui@gmail.com>
2013-05-15 09:58:09 -0700

    reconstructing local repo

---
+++ parsing-gui/src/fna/parsing/PhraseComparable.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package fna.parsing;
+
+import java.util.Comparator;
+
+/**
+ * @author updates
+ *
+ */
+public class PhraseComparable implements Comparator<String> {
+
+	
+	public int compare(String string1, String string2){
+		string1 = string1.trim();
+		string2 = string2.trim();
+		int i = string1.split("\\s+").length;
+		int j = string2.split("\\s+").length;
+		if(i == j ) return 0;
+		else if(i < j) return 1;
+		else return -1;		
+	}
+
+
+
+}
