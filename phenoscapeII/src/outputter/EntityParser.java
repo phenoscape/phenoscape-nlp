aae3eb6e20934760dd4039fa3485983710fc377f
hongcui <hong1.cui@gmail.com>
2013-05-30 11:24:14 -0700

    Swartz 1004.xml checked

--- phenoscapeII/src/outputter/EntityParser.java
+++ phenoscapeII/src/outputter/EntityParser.java
@@ -23,31 +23,34 @@ public class EntityParser {
 	 */
 	public EntityParser(Element statement, Element root, boolean keyelement) {
 		//add all structures which are not "whole organism" to key structure
-				try{
-					List<Element> structures = XMLNormalizer.pathNonWholeOrganismStructure.selectNodes(statement);
-					ArrayList<String> RelatedStructures = new ArrayList<String>();
-					for(Element structure: structures){
-						//Hashtable<String, String> keyentity = new Hashtable<String, String>();
-						int flag=1;
-						String sid = structure.getAttributeValue("id");
-						
-						for(String Structid:RelatedStructures)
-							if(Structid.equals(sid))
-								flag=0;
-						
-						if(flag==1)		
+		try{
+			List<Element> structures = XMLNormalizer.pathNonWholeOrganismStructure.selectNodes(statement);
+			ArrayList<String> RelatedStructures = new ArrayList<String>(); //keep a record on related entities, which should not be processed again
+			for(Element structure: structures){
+				//Hashtable<String, String> keyentity = new Hashtable<String, String>();
+				int flag=1; //not being processed as a related entity
+				String sid = structure.getAttributeValue("id");
+
+				for(String Structid:RelatedStructures){
+					if(Structid.equals(sid)){
+						flag=0;
+						break;
+					}
+				}
+
+				if(flag==1)		
+				{
+					List<Element> relations = Utilities.relationWithStructureAsSubject(sid, root);
+					if((relations==null)||(relations.size()==0)) {//the structure is not related to others, form a simple entity
+						String sname = Utilities.getStructureName(root, sid);
+						EntityProposals entity = new EntitySearcherOriginal().searchEntity(root, sid, sname, "", sname, "");
+						if(entity!=null){
+							entities.add(entity);
+						}
+					}else{
+						for(Element relation:relations)
 						{
-							List<Element> relations = Utilities.relationWithStructureAsSubject(sid, root);
-						if((relations==null)||(relations.size()==0)) {//the structure is not related to others, form a simple entity
-							String sname = Utilities.getStructureName(root, sid);
-							EntityProposals entity = new EntitySearcherOriginal().searchEntity(root, sid, sname, "", sname, "");
-							if(entity!=null){
-								entities.add(entity);
-							}
-						}else{
-							for(Element relation:relations)
-							{
-								boolean negation = false;
+							boolean negation = false;
 							if(relation.getAttribute("negation")!=null) negation = Boolean.valueOf(relation.getAttributeValue("negation"));
 							RelationHandler rh = new RelationHandler(root, 
 									relation.getAttributeValue("name"),  
@@ -61,15 +64,16 @@ public class EntityParser {
 							if(entity!=null){
 								entities.add(entity);
 								for(Entity related:entity.getProposals())
-								if(related instanceof CompositeEntity)
-									RelatedStructures.add(relation.getAttributeValue("to"));
+									if(related instanceof CompositeEntity)
+										RelatedStructures.add(relation.getAttributeValue("to"));
 							}
-							}
-						}}
-					}				
-				}catch(Exception ex){
-					ex.printStackTrace();
-				}	
+						}
+					}
+				}
+			}				
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}	
 	}
 
 	public ArrayList<EntityProposals> getEntities() {
