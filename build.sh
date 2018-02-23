95540ef631383d8d47d3ca6e91ba790ebda0b800
Hilmar Lapp <hlapp@drycafe.net>
2018-02-23 12:46:40 -0500

    Builds both subproject JARs separately
    Although the POM for a Maven-based build of the phenoscapeII subproject is
    left in place here, the overall gains from it are minimal, for several
    reasons:
    - To run the application, both subproject JARs need to be built, and they
      reuse some of the same dependencies. Building only one through Maven and
      the other not yields little to no benefit for the overall list of
      dependencies to store and manage.
    - To run the application, all the dependency JARs are still needed at runtime.
      We could instruct Maven to fold each dependency along with the local
      classes all into a single self-contained JAR, but that would require to
      build everything through maven to be really effedctive. And it would
      also require producing platform-specific packages.
    - Our objective here is not to create a distributable and executable
      self-contained application, but to have a mechanism to build and run
      the application reproducibly and platform-independently.
    
    Hence, we're going back to simply using the jdk tools to compile and
    package, and require platform-specific dependencies to be provided by the
    user.

--- build.sh
+++ build.sh
@@ -1,36 +1,49 @@
 #!/usr/bin/env bash
 
-BUILDDIR=build
-CLASSESDIR=${BUILDDIR}/classes
-TARGETJAR=gui/SemanticCharaParser.jar
-PARSING_GUI_SRC=parsing-gui/src
-PHENOSCAPE_II_SRC=phenoscapeII/src
-JAVA_LIB_DIR=gui/SemanticCharaParser_lib
+set -e
+
+PARSING_GUI_BASE=parsing-gui
+PARSING_GUI_JAR=${PARSING_GUI_BASE}/parsing-gui.jar
+PHENOSCAPE_II_BASE=phenoscapeII
+PHENOSCAPE_II_JAR=${PHENOSCAPE_II_BASE}/phenoscapeII.jar
 MAIN_CLASS=fna.parsing.MainForm
+SWT_CLASSPATH=${SWT_CLASSPATH:-${CLASSPATH}}
 
-# create buil directory if it doesn't exist yet
-echo "==> creating build directory if it doesn't exist yet"
-if [ ! -d $CLASSESDIR ] ; then mkdir -p $CLASSESDIR ; fi
-
-# compile all java source files to the classes build directory
-echo "==> compiling java classes"
-find . -name "*.java" | xargs \
-    javac -sourcepath ${PARSING_GUI_SRC}:${PHENOSCAPE_II_SRC} \
-          -d ${CLASSESDIR} \
+echo "==> building ${PHENOSCAPE_II_BASE}"
+echo "    o creating build directory if it doesn't exist yet"
+if [ ! -d ${PHENOSCAPE_II_BASE}/classes ] ; then
+    mkdir -p ${PHENOSCAPE_II_BASE}/classes
+fi
+echo "    o compiling java classes"
+find ${PHENOSCAPE_II_BASE}/src -name "*.java" | xargs \
+    javac -sourcepath ${PHENOSCAPE_II_BASE}/src \
+          -d ${PHENOSCAPE_II_BASE}/classes \
           -encoding windows-1252 \
-          -classpath "${JAVA_LIB_DIR}/*"
-
-# copy additional files for packaging into the final jar
-echo "==> copying other files into build directory"
-(cd ${PARSING_GUI_SRC}; \
-    find . -type f ! -name *.java -exec cp -p "{}" ../../${CLASSESDIR}/"{}" \; )
+          -classpath "${PHENOSCAPE_II_BASE}/lib/*"
+echo "    o copying other files into classes directory"
 for f in log4j.properties XMLschemas ; do
-    cp -p -R ${PHENOSCAPE_II_SRC}/$f ${CLASSESDIR}
+    cp -p -R ${PHENOSCAPE_II_BASE}/src/main/java/$f ${PHENOSCAPE_II_BASE}/classes
 done
+echo "    o creating ${PHENOSCAPE_II_JAR}"
+jar cf ${PHENOSCAPE_II_JAR} -C ${PHENOSCAPE_II_BASE}/classes .
 
-# create final jar file
-echo "==> creating ${TARGETJAR}"
-jar cfe ${TARGETJAR} fna.parsing.MainForm -C ${CLASSESDIR} .
+echo "==> building ${PARSING_GUI_BASE}"
+echo "    o creating build directory if it doesn't exist yet"
+if [ ! -d ${PARSING_GUI_BASE}/classes ] ; then
+    mkdir -p ${PARSING_GUI_BASE}/classes
+fi
+echo "    o compiling java classes"
+find ${PARSING_GUI_BASE}/src -name "*.java" | xargs \
+    javac -sourcepath ${PARSING_GUI_BASE}/src \
+          -d ${PARSING_GUI_BASE}/classes \
+          -encoding windows-1252 \
+          -classpath "${SWT_CLASSPATH}:${PHENOSCAPE_II_JAR}:${PHENOSCAPE_II_BASE}/lib/*:${PARSING_GUI_BASE}/lib/*"
+echo "    o copying other files into classes directory"
+(cd ${PARSING_GUI_BASE}/src; \
+    find . -type f ! -name *.java -exec cp -p "{}" ../classes/"{}" \; )
+
+echo "    o creating ${PARSING_GUI_JAR}"
+jar cfe ${PARSING_GUI_JAR} ${MAIN_CLASS} -C ${PARSING_GUI_BASE}/classes .
 
 # done
 echo "==> Done."
