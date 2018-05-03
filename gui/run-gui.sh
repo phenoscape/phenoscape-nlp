8c253496ce863a27251366967c65acaea6dc683b
Hilmar Lapp <hlapp@drycafe.net>
2018-05-03 11:43:08 -0400

    Adds comments to build and run shell scripts

--- gui/run-gui.sh
+++ gui/run-gui.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 
+## location of the parsing-gui jar and its dependencies
 PARSING_GUI_JAR=../parsing-gui/parsing-gui.jar
 PARSING_GUI_LIB=../parsing-gui/lib/*
+
+## location of the phenoscapeII jar and its dependencies
 PHENOSCAPE_II_JAR=../phenoscapeII/phenoscapeII.jar
 PHENOSCAPE_II_LIB=../phenoscapeII/lib/*
+
+## the name of the class to run the application
 MAIN_CLASS=fna.parsing.MainForm
+
+## the path for finding the MySQL JDBC driver jar
 JDBC_CLASSPATH=${JDBC_CLASSPATH:-lib/mysql-connector-java-5.0.8-bin.jar}
 
-JAVA_OPTIONS=-XstartOnFirstThread
+## application-specific options to be passed to the JRE
+##  -XstartOnFirstThread may be needed for SWT, depending on OS
+##  -DentityExpansionLimit is used by the gui application
+## you may also need to increase the heap size
+JAVA_OPTIONS=-XstartOnFirstThread -DentityExpansionLimit=100000000
+
+## the path to the SWT jar(s) must be either in SWT_CLASSPATH, or in CLASSPATH
+
+##################################################
+## should not need to customize any of the below #
+##################################################
 
 _CP=${PARSING_GUI_JAR}:${PARSING_GUI_LIB}:${PHENOSCAPE_II_JAR}:${PHENOSCAPE_II_LIB}
 
